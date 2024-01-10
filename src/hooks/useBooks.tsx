9ebce31d3cad6782327cb3c319cd1a05f350c296
Leonel <leonelroman321@gmail.com>
2024-01-10 15:30:49 -0300

    added info of availible books and amount of books in reading list

--- src/hooks/useBooks.tsx
+++ src/hooks/useBooks.tsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
 import { Book } from "../interfaces";
 
-export default function useBooks () {
+export default function useBooks() {
+  const [readingList, setReadingList] = useState<Array<Book>>([]);
 
-  const [readingList, setReadingList] = useState<Array<Book>>([])
-  
   const setBook = (book: Book) => {
+    // verificar si existe una lista anterior
+    const readingList =
+      window.localStorage.getItem("readingList") &&
+      JSON.parse(window.localStorage.getItem("readingList")!);
 
-    const readingList = window.localStorage.getItem('readingList') && JSON.parse(window.localStorage.getItem('readingList')!)
+    // si no existe una lista de lectura crear una nueva
+    if (!readingList) {
+      const firstReadingList = [book]
 
-    const newReadingList: Array<Book> = [...readingList, book]
+      window.localStorage.setItem('readingList', JSON.stringify(firstReadingList))
 
-    window.localStorage.setItem('readingList', JSON.stringify(newReadingList))
-    
-    setReadingList(newReadingList)
+      setReadingList(firstReadingList)
 
-  }
+      return
+
+    }
+    const newReadingList: Array<Book> = [...readingList, book];
+
+    window.localStorage.setItem("readingList", JSON.stringify(newReadingList));
+
+    setReadingList(newReadingList);
+  };
 
   return {
     setBook,
-    readingList
-  }
-
+    readingList,
+  };
 }
